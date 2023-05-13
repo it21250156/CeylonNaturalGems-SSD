fe5cbdeaf0766c92002a443c63ed58e300b01f33
it21053696 <it21053696@my.sliit.lk>
2023-05-13 14:47:26 +0530

    jewellere change

--- frontend/src/pages/JewelleryAdminDashboard.js
+++ frontend/src/pages/JewelleryAdminDashboard.js
@@ -1,85 +1,142 @@
-import { useEffect, useState } from "react"
-import { useJewelleryesContext } from '../hooks/useJewelleryesContext'
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useJewelleryesContext } from "../hooks/useJewelleryesContext";
+import { useNavigate } from "react-router-dom";
 
-
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useAuthContext } from "../hooks/useAuthContext";
 import React from "react";
 
-import { Link } from 'react-router-dom';
-import { useLogout } from '../hooks/useLogout';
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import axios from "axios";
 
 // components
-import JewelleryDetails from "../components/JewelleryDetails"
-import JewelleryAddForm from "../components/JewelleryAddForm"
+import JewelleryDetails from "../components/JewelleryDetails";
+import JewelleryAddForm from "../components/JewelleryAddForm";
 
 const JewelleryAdminDashboard = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState("Male");
+  const [filteredJwl, setFilteredJwl] = useState([])
+
+  const handleButtonClick = (gender) => {
+    setActiveButton(gender);
+
+  };
+
+  const buttonStyle = {
+    padding: "10px 20px",
+    margin: "5px",
+    borderRadius: "5px",
+    border: "none",
+    backgroundColor: "#e6e6e6",
+    color: "#333",
+  };
+
+  const activeButtonStyle = {
+    backgroundColor: "#007bff",
+    color: "#fff",
+  };
 
   const { logout } = useLogout();
-    const {user} = useAuthContext()
-    const handleClick = () => {
-      logout();
-      navigate('/');
-    };
+  const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+    navigate("/");
+  };
 
   //const {jewellery, dispatch} = useJewelleryesContext()
-  const [jewellery , setjewellery] = useState(null)
+  const [jewellery, setjewellery] = useState(null);
 
   useEffect(() => {
     const fetchJewelleryes = async () => {
-      const response = await fetch('/api/jewelleryes')
-      const json = await response.json()
+      const response = await fetch("/api/jewelleryes");
+      const json = await response.json();
 
       if (response.ok) {
         //dispatch({type: 'SET_JEWELLERY', payload: json})
-        setjewellery(json)
+        setjewellery(json);
+        setFilteredJwl(json.filter(jwl  => jwl.gender == activeButton))
       }
-    }
+    };
 
-    fetchJewelleryes()
-  }, [])
+    fetchJewelleryes();
+
+  }, [activeButton]);
 
   return (
     <>
       <header>
-      <div>
-        <div className="background">
-          <div className="headerNameDiv">
-            <h1 className="headerName">Ceylon Natural Gems</h1>
+        <div>
+          <div className="background">
+            <div className="headerNameDiv">
+              <h1 className="headerName">Ceylon Natural Gems</h1>
+            </div>
           </div>
-        </div>
 
-        <nav>
-          <div className="navprofileDiv">
+          <nav>
+            <div className="navprofileDiv">
               <div className="navEmal">
-                <span>
-                  Hello Admin
-                </span>
+                <span>Hello Admin</span>
                 <button onClick={handleClick}>Log out</button>
               </div>
+            </div>
+
+            <ul>
+              <li>
+                <Link to={"/adminHome"}>Home</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="home">
+        {/* <JewelleryAddForm/> */}
+        <button
+          onClick={() => {
+            navigate("/AddJewelleryes");
+          }}
+        >
+          Add a new jewellery
+        </button>
+        <div className="jewelleryes">
+          <div className="gender-switch">
+            <button
+              type="button"
+              style={
+                activeButton === "Male"
+                  ? { ...buttonStyle, ...activeButtonStyle }
+                  : buttonStyle
+              }
+              onClick={() => handleButtonClick("Male")}
+            >
+              Men
+            </button>
+            <button
+              type="button"
+              style={
+                activeButton === "Female"
+                  ? { ...buttonStyle, ...activeButtonStyle }
+                  : buttonStyle
+              }
+              onClick={() => handleButtonClick("Female")}
+            >
+              Women
+            </button>
           </div>
 
-          <ul>
-            <li>
-              <Link to={'/adminHome'}>Home</Link>
-            </li>
-          </ul>
-        </nav>
+          {/* {jewellery &&
+            jewellery.map((jewellery) => (
+              <JewelleryDetails jewellery={jewellery} key={jewellery._id} />
+            ))} */}
+          {filteredJwl &&
+            filteredJwl.map((jewellery) => (
+              <JewelleryDetails jewellery={jewellery} key={jewellery._id} />
+            ))}
+        </div>
       </div>
-    </header>
-    <div className="home">
-      <div className="jewelleryes">
-        {jewellery && jewellery.map(jewellery => (
-          <JewelleryDetails jewellery={jewellery} key={jewellery._id} />
-        ))}
-      </div>
-      {/* <JewelleryAddForm/> */}
-      <button onClick={() => {navigate("/AddJewelleryes")}}>Add a new jewellery</button>
-    </div>
+      <style></style>
     </>
-  )
-}
+  );
+};
 
-export default JewelleryAdminDashboard
+export default JewelleryAdminDashboard;
