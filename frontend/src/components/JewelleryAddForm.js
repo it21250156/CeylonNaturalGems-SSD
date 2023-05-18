77e33962b31d14f59a6ed0321b79a187934b77c5
it21250156 <it21250156@my.sliit.lk>
2023-05-18 23:44:04 +0530

    jewel add form css

--- frontend/src/components/JewelleryAddForm.js
+++ frontend/src/components/JewelleryAddForm.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import TextareaAutosize from "react-textarea-autosize";
-import { useJewelleryesContext } from "../hooks/useJewelleryesContext";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import TextareaAutosize from 'react-textarea-autosize';
+import { useJewelleryesContext } from '../hooks/useJewelleryesContext';
 
 const JewelleryAddForm = () => {
   const { dispatch } = useJewelleryesContext();
 
-  const [name, setName] = useState("");
-  const [type, setType] = useState("");
-  const [gender, setGender] = useState("");
-  const [gemstone, setGemstone] = useState("");
-  const [metal, setMetal] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [error, setError] = useState("");
+  const [name, setName] = useState('');
+  const [type, setType] = useState('');
+  const [gender, setGender] = useState('');
+  const [gemstone, setGemstone] = useState('');
+  const [metal, setMetal] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [error, setError] = useState('');
   const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
@@ -23,56 +23,56 @@ const JewelleryAddForm = () => {
 
     const file = e.target.image.files[0];
     if (!file) {
-      setError("Please select an image.");
+      setError('Please select an image.');
     }
 
     const formData = new FormData();
-    formData.append("image", e.target.image.files[0]);
-    formData.append("name", name);
-    formData.append("type", type);
-    formData.append("gender", gender);
-    formData.append("metal", metal);
-    formData.append("gemstone", gemstone);
-    formData.append("price", price);
-    formData.append("description", description);
+    formData.append('image', e.target.image.files[0]);
+    formData.append('name', name);
+    formData.append('type', type);
+    formData.append('gender', gender);
+    formData.append('metal', metal);
+    formData.append('gemstone', gemstone);
+    formData.append('price', price);
+    formData.append('description', description);
 
     const emptyFields = [];
 
-    if (name === "") {
-      emptyFields.push("name");
+    if (name === '') {
+      emptyFields.push('name');
     }
 
-    if (type === "") {
-      emptyFields.push("type");
+    if (type === '') {
+      emptyFields.push('type');
     }
 
-    if (gender === "") {
-      emptyFields.push("gender");
+    if (gender === '') {
+      emptyFields.push('gender');
     }
 
-    if (metal === "") {
-      emptyFields.push("metal");
+    if (metal === '') {
+      emptyFields.push('metal');
     }
 
-    if (description === "") {
-      emptyFields.push("description");
+    if (description === '') {
+      emptyFields.push('description');
     }
 
-    if (gemstone === "") {
-      emptyFields.push("gemstone");
+    if (gemstone === '') {
+      emptyFields.push('gemstone');
     }
 
-    if (price === "") {
-      emptyFields.push("price");
+    if (price === '') {
+      emptyFields.push('price');
     }
 
     if (emptyFields.length > 0) {
       setEmptyFields(emptyFields);
-      setError("Please fill in all required fields.");
+      setError('Please fill in all required fields.');
     }
 
-    const response = await fetch("/api/jewelleryes", {
-      method: "POST",
+    const response = await fetch('/api/jewelleryes', {
+      method: 'POST',
       body: formData,
     });
 
@@ -83,27 +83,28 @@ const JewelleryAddForm = () => {
     }
 
     if (response.ok) {
-      setName("");
-      setType("");
-      setGender("");
-      setMetal("");
-      setGemstone("");
-      setDescription("");
-      setPrice("");
+      setName('');
+      setType('');
+      setGender('');
+      setMetal('');
+      setGemstone('');
+      setDescription('');
+      setPrice('');
       setError(null);
       setEmptyFields([]);
-      window.alert("Jewelleres was successfully add to the database!");
-      console.log("New jewellery Added", json);
-      dispatch({ type: "CREATE_jewellery", payload: json });
+      window.alert('Jewelleres was successfully add to the database!');
+      console.log('New jewellery Added', json);
+      dispatch({ type: 'CREATE_jewellery', payload: json });
     }
-    navigator("/JewelleryAdminDashbord");
+    navigator('/JewelleryAdminDashbord');
   };
 
   return (
     <form className="create" onSubmit={handleSubmit}>
-      <h3>Add a New Jewellery</h3>
+      <div className="darkBlueTopicBox">
+        <h3 className="pageTopic">Add a New Jewellery</h3>
+      </div>
 
-      <hr className="jewhr"></hr>
       <div className="jewinputbox">
         <label className="jewAddlabel">Jewellery Name: </label>
         <div className="jewInput">
@@ -112,9 +113,9 @@ const JewelleryAddForm = () => {
             onChange={(e) => setName(e.target.value)}
             value={name}
             //className="jewInput"
-            className={emptyFields.includes("name") ? "error" : ""}
+            className={emptyFields.includes('name') ? 'error' : ''}
           />
-          {emptyFields.includes("name") && (
+          {emptyFields.includes('name') && (
             <div className="error">Please enter a name.</div>
           )}
 
@@ -124,9 +125,9 @@ const JewelleryAddForm = () => {
             onChange={(e) => setType(e.target.value)}
             value={type}
             //className="jewInput"
-            className={emptyFields.includes("type") ? "error" : ""}
+            className={emptyFields.includes('type') ? 'error' : ''}
           />
-          {emptyFields.includes("type") && (
+          {emptyFields.includes('type') && (
             <div className="error">Please enter a type.</div>
           )}
 
@@ -139,9 +140,9 @@ const JewelleryAddForm = () => {
             <option value="">Select a Gender</option>
             <option value="Male">Men</option>
             <option value="Female">Women</option>
-            className={emptyFields.includes("gender") ? "error" : ""}
+            className={emptyFields.includes('gender') ? 'error' : ''}
           </select>
-          {emptyFields.includes("gender") && (
+          {emptyFields.includes('gender') && (
             <div className="error">Please enter a gender.</div>
           )}
 
@@ -154,9 +155,9 @@ const JewelleryAddForm = () => {
             <option value="">Select a Metal</option>
             <option value="GOLD">GOLD</option>
             <option value="SILVER">SILVER</option>
-            className={emptyFields.includes("metal") ? "error" : ""}
+            className={emptyFields.includes('metal') ? 'error' : ''}
           </select>
-          {emptyFields.includes("Metal") && (
+          {emptyFields.includes('Metal') && (
             <div className="error">Please selecta metal.</div>
           )}
 
@@ -177,9 +178,9 @@ const JewelleryAddForm = () => {
             <option value="Tourmaline">Tourmaline</option>
             <option value="Turquoies">Turquoies</option>
             <option value="Cat's Eye">Cat's Eye</option>
-            className={emptyFields.includes("gemstone") ? "error" : ""}
+            className={emptyFields.includes('gemstone') ? 'error' : ''}
           </select>
-          {emptyFields.includes("Gemstone") && (
+          {emptyFields.includes('Gemstone') && (
             <div className="error">Please select a gemstone.</div>
           )}
 
@@ -191,10 +192,10 @@ const JewelleryAddForm = () => {
             type="textarea"
             onChange={(e) => setDescription(e.target.value)}
             value={description}
-            className={emptyFields.includes("description") ? "error" : ""}
+            className={emptyFields.includes('description') ? 'error' : ''}
           />
 
-          {emptyFields.includes("description") && (
+          {emptyFields.includes('description') && (
             <div className="error">Please enter a description.</div>
           )}
 
@@ -204,9 +205,9 @@ const JewelleryAddForm = () => {
             onChange={(e) => setPrice(e.target.value)}
             value={price}
             // className="jewInput"
-            className={emptyFields.includes("price") ? "error" : ""}
+            className={emptyFields.includes('price') ? 'error' : ''}
           />
-          {emptyFields.includes("price") && (
+          {emptyFields.includes('price') && (
             <div className="error">Please enter a type.</div>
           )}
         </div>
@@ -215,13 +216,18 @@ const JewelleryAddForm = () => {
         <br />
 
         <label className="gem-label">Jewellery Image:</label>
-        <input type="file" name="image" accept="image/*" />
+        <input
+          className="jewel-img-input"
+          type="file"
+          name="image"
+          accept="image/*"
+        />
       </div>
       <br />
       <br />
       <br />
       <br />
-      {emptyFields.includes("description") && (
+      {emptyFields.includes('description') && (
         <div className="error">Please enter a description.</div>
       )}
 
@@ -235,7 +241,7 @@ const JewelleryAddForm = () => {
             </Link> */}
       <button
         className="baktohomebtn"
-        onClick={() => (window.location.href = "/JewelleryAdminDashboard")}
+        onClick={() => (window.location.href = '/JewelleryAdminDashboard')}
       >
         Back to Home
       </button>
