b28469d5ef95080449f7f0b2676b37d07d074fdb
it21250156 <it21250156@my.sliit.lk>
2023-05-19 04:06:08 +0530

    my installments css

--- frontend/src/pages/MyInstallmentsDetailed.js
+++ frontend/src/pages/MyInstallmentsDetailed.js
@@ -1,49 +1,56 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
-import { useAuthContext } from "../hooks/useAuthContext"
-import React from "react";
-import { useNavigate } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 
-import "../CSS/Vih's.css"
+import "../CSS/Vih's.css";
 import Header from '../components/Header';
 
 const MyInstallmentsDetailed = () => {
+  const { id } = useParams();
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
 
-    const {id} = useParams();
-    const {user} = useAuthContext()
-    const navigate = useNavigate()
-    
-    const [installment , setInstallment] = useState({
-        user:"" , gemID:"", planID:"", noOfMonths:"", totalAmount:"", initialPayment:"", monthlyPayment:"", installmentDates:[], status:""
-    })
+  const [installment, setInstallment] = useState({
+    user: '',
+    gemID: '',
+    planID: '',
+    noOfMonths: '',
+    totalAmount: '',
+    initialPayment: '',
+    monthlyPayment: '',
+    installmentDates: [],
+    status: '',
+  });
 
-    const [gems, setGems] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [plans, setPlans] = useState([]);
+  const [gems, setGems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [plans, setPlans] = useState([]);
 
+  useEffect(() => {
+    const fetchInstallments = async () => {
+      const response = await fetch(`/api/installments/${id}`);
+      const json = await response.json();
 
-    useEffect(() => {
-        const fetchInstallments = async () => {
-            const response = await fetch(`/api/installments/${id}`)
-            const json = await response.json()
+      if (response.ok) {
+        setInstallment(json);
+      }
+    };
 
-            if(response.ok){
-                setInstallment(json)
-            }
-        }
-
-        fetchInstallments()
-    },[])
-
+    fetchInstallments();
+  }, []);
 
   useEffect(() => {
     const fetchGems = async () => {
       try {
-        const response = await fetch(`/api/gems&jewelleries/gems/${installment.gemID}`);
+        const response = await fetch(
+          `/api/gems&jewelleries/gems/${installment.gemID}`
+        );
         const json = await response.json();
         setGems(json);
       } catch (err) {
@@ -62,14 +69,14 @@ const MyInstallmentsDetailed = () => {
     };
 
     const fetchPlans = async () => {
-        try {
-          const response = await fetch('/api/plans');
-          const json = await response.json();
-          setPlans(json);
-        } catch (err) {
-          console.log(err);
-        }
-      };
+      try {
+        const response = await fetch('/api/plans');
+        const json = await response.json();
+        setPlans(json);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
     fetchPlans();
     fetchGems();
@@ -78,51 +85,72 @@ const MyInstallmentsDetailed = () => {
 
   const usersName = users.find((user) => user._id === installment.user);
 
-    return (
-        <>
-        <Header/>
+  return (
+    <>
+      <Header />
 
-    <div className="lightBlueBodyBG">
-        <div className="lightBlueBox" >
-            <div className="img-section-large">
-                <div className="gem-image-container">
-                    <img
-                      src={gems.find((gem) => gem._id === installment.gemID)?.gem_img}
-                      alt="Gem"
-                      className="gem-card__image"
-                    />
-                </div>
-              </div>
-            <h3>{gems.find((gem) => gem._id === installment.gemID)?.name} </h3>
-            <h3>{gems.find((gem) => gem._id === installment.gemID)?.color} </h3>
-        </div>
-        <div className="whiteBox">
-            <br></br>
-            
-            <table className="table table-striped table-hover">
-                <tr><td>Purchased Date</td>
-                <td>{new Date(installment.installmentDates[0]).toLocaleDateString()}</td>
-                </tr>
-                <tr><td>Installement Plan</td> <td>{plans.find((plan) => plan._id === installment.planID)?.name}</td></tr>
-                <tr><td>Total Amount</td> <td>{installment.totalAmount}</td></tr>
-                <tr><td>Initial Payment</td> <td>{installment.initialPayment}</td></tr>
-                <tr><td>Monthly Payment</td> <td>{installment.monthlyPayment}</td></tr>
-                <tr><td>Total Number of Months to Pay</td> <td>{installment.noOfMonths}</td></tr>
+      <div className="lightBlueBox">
+        <div className="top-box-inst">
+          <div className="top-left-box">
+            <h3 className="gem-name-inst">
+              {gems.find((gem) => gem._id === installment.gemID)?.name}{' '}
+            </h3>
+            <img
+              src={gems.find((gem) => gem._id === installment.gemID)?.gem_img}
+              alt="Gem"
+              className="gem-card__image"
+            />
+          </div>
+          <div className="top-right-box">
+            <table className="table table-striped table-hover inst-table-1">
+              <tr>
+                <td className="top-table-labels">Purchased Date</td>
+                <td>
+                  {new Date(
+                    installment.installmentDates[0]
+                  ).toLocaleDateString()}
+                </td>
+              </tr>
+              <tr>
+                <td className="top-table-labels">Installement Plan</td>{' '}
+                <td>
+                  {plans.find((plan) => plan._id === installment.planID)?.name}
+                </td>
+              </tr>
+              <tr>
+                <td className="top-table-labels">Total Amount</td>{' '}
+                <td>{installment.totalAmount}</td>
+              </tr>
+              <tr>
+                <td className="top-table-labels">Initial Payment</td>{' '}
+                <td>{installment.initialPayment}</td>
+              </tr>
+              <tr>
+                <td className="top-table-labels">Monthly Payment</td>{' '}
+                <td>{installment.monthlyPayment}</td>
+              </tr>
+              <tr>
+                <td className="top-table-labels">
+                  Total Number of Months to Pay
+                </td>{' '}
+                <td>{installment.noOfMonths}</td>
+              </tr>
 
-                <br></br>
-                <tr><td>Status</td> <td>{installment.status}</td></tr>
-
+              <br></br>
+              <tr>
+                <td className="top-table-labels">Status</td>{' '}
+                <td>{installment.status}</td>
+              </tr>
             </table>
+          </div>
+        </div>
 
-            
-        </div> 
-        <div className="whiteBox">
-
+        <div className="white-box-inst">
           <h3>Payment Details</h3>
           <hr></hr>
-            
-          <table className="table table-striped table-hover" >
-            <thead> 
+
+          <table className="table table-striped table-hover">
+            <thead>
               <tr>
                 <th>Month</th>
                 <th>Date of the Payemnt</th>
@@ -142,23 +170,19 @@ const MyInstallmentsDetailed = () => {
               ))}
             </tbody>
           </table>
-
-          <button 
-            onClick={() => {
-                navigate('/payments');
-                localStorage.setItem('TamountInfo', installment.monthlyPayment);
-                }} > 
-                
-                Do next payment 
+        </div>
+        <button
+          className="nxt-payment-btn"
+          onClick={() => {
+            navigate('/payments');
+            localStorage.setItem('TamountInfo', installment.monthlyPayment);
+          }}
+        >
+          Do next payment
         </button>
-            
-        </div> 
-    </div>
+      </div>
     </>
-    )
-}
+  );
+};
 
-
-
-
-export default MyInstallmentsDetailed
+export default MyInstallmentsDetailed;
