b08cacf69ac4c23d08739020c721d3db940f1126
IT21303852 <it21303852@my.sliit.lk>
2023-05-21 12:16:50 +0530

    bored

--- frontend/src/components/GemAdminPriceReport.js
+++ frontend/src/components/GemAdminPriceReport.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-import { useGemsContext } from "../hooks/useGemsContext";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useGemsContext } from '../hooks/useGemsContext';
+import { Bar } from 'react-chartjs-2';
 
-import "../CSS/GemAdminReports.css";
+import '../CSS/GemAdminReports.css';
 
 const GemAdminReports = () => {
     const { gems } = useGemsContext();
@@ -9,14 +11,14 @@ const GemAdminReports = () => {
 
     useEffect(() => {
         const calculateAverages = () => {
-            let allGemsData = { type: "All", avgPrice: 0, minPrice: 0, maxPrice: 0 };
+            let allGemsData = { type: 'All', avgPrice: 0, minPrice: 0, maxPrice: 0 };
 
             if (gems.length > 0) {
                 const prices = gems.map((gem) => gem.price);
                 const avgPrice = prices.reduce((acc, curr) => acc + curr, 0) / prices.length;
                 const minPrice = Math.min(...prices);
                 const maxPrice = Math.max(...prices);
-                allGemsData = { type: "All", avgPrice, minPrice, maxPrice };
+                allGemsData = { type: 'All', avgPrice, minPrice, maxPrice };
             }
 
             const types = [...new Set(gems.map((gem) => gem.type))];
@@ -34,29 +36,24 @@ const GemAdminReports = () => {
         calculateAverages();
     }, [gems]);
 
+    const chartData = {
+        labels: gemData.map((gem) => gem.type),
+        datasets: [
+            {
+                label: 'Average Price',
+                data: gemData.map((gem) => gem.avgPrice.toFixed(2)),
+                backgroundColor: ['#36a2eb', '#ff6384', '#cc65fe', '#ffcd56'], // Add more colors as needed
+            },
+        ],
+    };
+
     return (
         <div className="reports-gem-admin">
 
-            <table className="report-table">
-                <thead>
-                    <tr>
-                        <th className="gem-admin-report-header">Type</th>
-                        <th className="gem-admin-report-header">Average Price</th>
-                        <th className="gem-admin-report-header">Lowest Price</th>
-                        <th className="gem-admin-report-header">Highest Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {gemData.map((gem) => (
-                        <tr key={gem.type} className="report-row">
-                            <td className="gem-admin-report-cell">{gem.type}</td>
-                            <td className="gem-admin-report-cell">{gem.avgPrice.toFixed(2)}/=</td>
-                            <td className="gem-admin-report-cell">{gem.minPrice}/=</td>
-                            <td className="gem-admin-report-cell">{gem.maxPrice}/=</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="gem-admin-bar-chart-container">
+                <h3 className="admin-chart-title">Average Price by Gem Type</h3>
+                <Bar data={chartData} />
+            </div>
         </div>
     );
 };
--- frontend/src/components/TypeOfGemsReport.js
+++ frontend/src/components/TypeOfGemsReport.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useGemsContext } from '../hooks/useGemsContext';
+import { Pie } from 'react-chartjs-2';
 
 import '../CSS/GemAdminReports.css';
 
@@ -17,24 +18,30 @@ const GemTypesReport = () => {
 
   const gemTypes = Object.keys(typesCount);
 
+  const chartData = {
+    labels: gemTypes,
+    datasets: [
+      {
+        data: Object.values(typesCount),
+        backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffcd56'], // Add more colors as needed
+      },
+    ],
+  };
+
   return (
-    <div className="admin-table-container">
-      <table className="report-table">
-        <thead>
-          <tr>
-            <th className="gem-admin-report-header">Type</th>
-            <th className="gem-admin-report-header">Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gemTypes.map((type, index) => (
-            <tr key={type} className="report-row">
-              <td className="gem-admin-report-cell">{type}</td>
-              <td className="gem-admin-report-cell">{typesCount[type]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="admin-chart-container">
+      <h3 className="admin-chart-title">Gem Types Report</h3>
+      <div className="pie-chart-container">
+        <Pie data={chartData} />
+      </div>
+      <div className="legend-container">
+        {gemTypes.map((type, index) => (
+          <div className="legend-item" key={type}>
+            <span style={{ backgroundColor: chartData.datasets[0].backgroundColor[index] }}></span>
+            {type}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
